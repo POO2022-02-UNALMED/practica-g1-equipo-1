dacf02e390601786ee49b240c68f499b12d1b7f4
altamarluis <laltamar@unal.edu.co>
2022-10-25 15:05:28 -0500

    Update Bot.java

--- EscapeRoom/src/gestorAplicacion/clasesLogicas/Bot.java
+++ EscapeRoom/src/gestorAplicacion/clasesLogicas/Bot.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gestorAplicacion.clasesLogicas;
 
 import gestorAplicacion.clasesLogicasHerencia.Intruso;
 import gestorAplicacion.clasesLogicasHerencia.Robot;
 import uiMain.Main;
 
-/**
- *
- * @author LENOVO
- */
+
 public class Bot extends Robot{
     private static final int DUMPENER = 6;
     public Bot(){
