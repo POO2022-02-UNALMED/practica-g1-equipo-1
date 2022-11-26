5b8a08db873c2e7445bef130533e26106668bcd2
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2022-11-26 18:20:50 -0500

    cambios de imagen

--- baseDatos/serializador.py
+++ baseDatos/serializador.py
@@ -1,4 +1,6 @@
 import pickle
+
+#Logicas
 from python.gestorAplicacion.clasesLogicas.Ahorro import Ahorro
 from python.gestorAplicacion.clasesLogicas.Armas import Armas
 from python.gestorAplicacion.clasesLogicas.Habitacion import Habitacion
@@ -6,6 +8,8 @@ from python.gestorAplicacion.clasesLogicas.Individuo import Individuo
 from python.gestorAplicacion.clasesLogicas.Jarvis import Jarvis
 from python.gestorAplicacion.clasesLogicas.Objetos import Objetos
 from python.gestorAplicacion.clasesLogicas.Bot import Bot
+
+#herencia
 from python.gestorAplicacion.clasesLogicasHerencia.Robot import Robot
 from python.gestorAplicacion.clasesLogicasHerencia.Intruso import Intruso
 import pathlib
@@ -26,4 +30,4 @@ class Serializador():
             print("Falla al serializar")
 
     def serializarTodo():
-        Serializador.serializar(Ahorro)
+        serializar(Ahorro)
